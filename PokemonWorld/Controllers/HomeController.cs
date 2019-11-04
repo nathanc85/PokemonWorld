0e1eebbbcbd3db3a8997ff82f19d266d6c40c2d7
Nathan Costea <cnlnat@yahoo.com>
2019-11-04 11:16:34 -0500

    Called the API.

--- PokemonWorld/Controllers/HomeController.cs
+++ PokemonWorld/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +13,19 @@ namespace PokemonWorld.Controllers
     {
         public ActionResult Index()
         {
+            // Create the request to the API
+            WebRequest request = WebRequest.Create("https://pokeapi.co/api/v2/pokemon/1");
+            // Send the request.
+            WebResponse response = request.GetResponse();
+            // Get back the response stream.
+            Stream stream = response.GetResponseStream();
+            // Make the stream reacheable/accessible.
+            StreamReader reader = new StreamReader(stream);
+            // Human readable response.
+            string responseString = reader.ReadToEnd();
+
+            JObject parsedString = JObject.Parse(responseString);
+
             return View();
         }
     }
